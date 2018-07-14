c395f98e03f1a66e9fb16513aedc0b27af5c68f1
crimnut <agoota.90380@gmail.com>
2018-07-14 19:52:51 +0900

    modify Parallel processing using joblib
    but, raise error.
    cause joblib? TypeError
    i don't know....

--- ProcessImage/detect_and_save_facearea_with_face_api.py
+++ ProcessImage/detect_and_save_facearea_with_face_api.py
@@ -1,17 +1,14 @@
 """
 Using Azure FaceAPI, detect faces.
 save face image to output directory
-
-todo: 並列処理の実装
-todo:  ログ出力機能
-todo: 省メモリ
-
 """
 import requests
 import cv2
 import glob
 import os
 import dotenv
+from datetime import datetime
+from joblib import Parallel, delayed
 
 
 def call_api(binary_image, api_key):
@@ -54,28 +51,60 @@ def save_face_area(image_file_path, x_, y_, w_, h_):
     # save face area as image file.
 
     image = cv2.imread(image_file_path)
-    file_name = image_file_path.split("\\")[2]
+    image_file_path = create_saving_path(image_file_path)
     image = image[x_:x_ + h_, y_:y_ + w_]  # 顔領域を切り出す
-    cv2.imwrite(".\\output\\" + file_name, image)
+    cv2.imwrite(image_file_path, image)
 
 
-if __name__ == "__main__":
-    path = os.path.join(os.path.dirname(__file__), ".env")
-    dotenv.load_dotenv(path)
-    API_KEY = os.environ.get("FACE_API_KEY")
+def make_dir(dir_path):
+    dir_path = dir_path.split("\\")[-1]
+    dir_path = os.path.join("..\\face_images", dir_path)
+    if not os.path.isdir(dir_path):
+        os.mkdir(dir_path)
 
-    file_path_list = glob.glob(".\\sample\\*")
+
+def create_saving_path(file_path):
+    file_path = "\\".join(file_path.split("\\")[2:])
+    file_path = os.path.join("..\\face_images", file_path)
+    return file_path
 
 
-def main(image_file_path, apk):
+def write_log(log_file, image_path, response):
+    with open(log_file, "a") as log:
+        log_text = datetime.strftime(datetime.now(), "<%Y-%m-%d %H:%M:%S>") + " " \
+                   + "Found_face " + f"[{image_path}] " + "Result " + str(response) + "\n"
+        log.write(log_text)
+
+
+def main_process(image_file_path, apk):
+    LOG_FILE = ".\\save_faces_log.txt"
+
     with open(image_file_path, "rb") as f:
         img = f.read()
         response = call_api(img, apk)
 
         if response == []:
             print(f"No result: {image_file_path}")
+            write_log(LOG_FILE, image_file_path, response)
         else:
             for r in response:
                 x, y, w, h = detect_face_areas(r)
                 save_face_area(image_file_path, x, y, w, h)
                 print(f"Save a face of {image_file_path}")
+                write_log(LOG_FILE, image_file_path, r)
+
+
+path = os.path.join(os.path.dirname(__file__), ".env")
+dotenv.load_dotenv(path)
+API_KEY = os.environ.get("FACE_API_KEY")
+
+file_path_list = glob.glob("..\\images\\*\\*")
+
+# Parallel(n_jobs=-1)([delayed(main_process)(file_path, API_KEY) for file_path in file_path_list])
+
+
+with open("..\\images\\akane_moriya\\02189_akane_moriya.jpg", "rb") as f:
+    img = f.read()
+    response = call_api(img, API_KEY)
+    for r in response:
+        print(r)
